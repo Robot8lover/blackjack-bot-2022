28db0ffffbaf70f468c2c1f2cf7af44ce54ff106
Robot8lover <108429990+Robot8lover@users.noreply.github.com>
2022-07-20 16:26:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+const getMoves = (decks, hand, dealerCard) => {
+  /**
+    * Steps:
+      * Make array of card numbers (2 through 11 or 1 through 10, count face cards as all 10s for now)
+      * Loop over each potential delear hidden card
+      * Determine expected value of hit and stand at each step
+      * Save results to an array to allow access at any point in tree (so recalculation is unecessary, ignore this at first)
+    */
+};
